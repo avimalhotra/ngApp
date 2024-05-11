a5f68b38055cf2da51a85a3a5820682bbaf35137
Avinash <avimalhotra505@gmail.com>
2024-05-11 17:07:12 +0530

    Angular Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ServerComponent } from './server/server.component';
 
 @Component({
-  selector: 'app-root',
+   selector: 'app-root',
+  // selector: '[app-root]',
+  //  selector: '.container',
+  //selector: '#app',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ServerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  //template:`<h1>This is {{title}}</h1>`,
+  //styles:['h1{ color:red }']
 })
 export class AppComponent {
   title = 'ngApp';
+  date= new Date().toLocaleString();
 }
