b3219264a7cd0f3196d33da6311a0e2891797b7e
Avinash <avimalhotra505@gmail.com>
2024-05-18 19:49:25 +0530

    two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 // import { ServerComponent } from './server/server.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet /* ServerComponent */ ],
+  imports: [RouterOutlet, FormsModule /* ServerComponent */ ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ngApp';
-  x=3;
-  date= new Date().toLocaleString();
-  cars=["swift","alto","brezza"];
-  prices=[10,2,12,1,3,21];
-  numSort=function(x:number,y:number){ return x-y };
-  car={name:"swift", power:90, "on-road":1000000};
-  toReverse=(x:string)=>{return x.split("").reverse().join("")};
-  area=(x:number, y:number)=> { return x*y } ;
-  src="assets/icon.png";
-  dis=true;
-  p="para";
-  dark={"background":"#222","color":"#fff"};
   name="";
-  sayHi(){ this.name="Hello" };
-  sayBye(){ this.name="bye Bye" };
+  //updateName(x:Event){this.name=(x.target as HTMLInputElement).value}
+  range=0;
+  chk=false;
+  gen="";
+  city="";
+  hybrid=true;
+  num=0;
 }
