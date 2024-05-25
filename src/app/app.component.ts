7e4fcc308a41723094ddf6f98d0897d5e8e4acff
Avinash <avimalhotra505@gmail.com>
2024-05-25 17:10:32 +0530

    ngFor, ngSwitch and pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 // import { ServerComponent } from './server/server.component';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { CustomPipe } from './custom.pipe';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule /* ServerComponent */ ],
+  imports: [RouterOutlet, FormsModule , CommonModule, CustomPipe /* ServerComponent */ ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -15,9 +18,27 @@ export class AppComponent {
   name="";
   //updateName(x:Event){this.name=(x.target as HTMLInputElement).value}
   range=0;
-  chk=false;
+  chk=true;
   gen="";
   city="";
   hybrid=true;
   num=0;
+  cars=["swift","alto","baleno"];
+  car={name:"swift", power:82};
+  data=[
+    {"name": "swift", "type": "hatchback", "price":810000},
+    {"name": "swift", "type": "hatchback", "price":810000},
+    {"name": "dzire", "type": "hatchback", "price":880000},
+    {"name": "ciaz", "type": "sedan", "price":1000000},
+    {"name": "baleno", "type": "hatchback", "price":850000},
+    {"name": "brezza", "type": "suv", "price":1400000},
+    {"name": "fronx", "type": "hatchback", "price":1000000},
+    {"name": "jimny", "type": "suv", "price":1300000}
+  ];
+  str="lorem ipsum";
+  day=1;
+  days=["sunday","monday","tuesday"];
+  css={'color':'red','text-align':'center'};
+  
+
 }
---
+++ src/app/custom.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+    
+@Pipe({
+    standalone:true,
+    name: 'custom'
+})
+    
+export class CustomPipe implements PipeTransform {
+        
+transform(x: string): string {
+    return x.charAt(0).toUpperCase() + x.substr(1).toLowerCase();
+} 
+} 
