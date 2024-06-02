3c3a42ba67e3bbe4d6012b7fd8d8a47e1c6f4002
Avinash <avimalhotra505@gmail.com>
2024-06-02 17:51:18 +0530

    Reactive Form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Validator, FormBuilder } from '@angular/forms';
 import { FormControl, FormGroup, FormArray } from '@angular/forms';
 
 
@@ -16,15 +17,27 @@ import { FormControl, FormGroup, FormArray } from '@angular/forms';
 export class AppComponent {
   title = 'ngApp';
 
-  name="Avi";
   //email = new FormControl('');
   //age = new FormControl('');
 
-  loginForm=new FormGroup({
-      email:new FormControl(`${this.name}`),
-      age:new FormControl(""),
-      pass:new FormControl(""),
+  /* loginForm=new FormGroup({
+      email:new FormControl("",[Validators.required, Validators.pattern(/[\w]{1,}\@[\w]{1,}\.[\w]{2,}/)]),
+      age:new FormControl("",[Validators.required,Validators.min(18),Validators.max(25)]),
+      pass:new FormControl("",[Validators.required,Validators.pattern(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,12})/)]),
+  }); */
+
+  constructor(private fb:FormBuilder){ }
+
+  loginForm:FormGroup=this.fb.group({
+    email:new FormControl("",[Validators.required, Validators.pattern(/[\w]{1,}\@[\w]{1,}\.[\w]{2,}/)]),
+    age:new FormControl("",[Validators.required,Validators.min(18),Validators.max(25)]),
+    pass:new FormControl("",[Validators.required,Validators.pattern(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,12})/)]),
   });
+
+
+  patchVal(){this.loginForm.patchValue({email:"user@domian.com"}) }
+  setVal(){this.loginForm.setValue({email:"user@domian.com",age:0, pass:'00000000'}) }
+
 
   submitForm(){
     console.log(this.loginForm.value);
