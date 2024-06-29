570e20942371de1abac3c724d8f9322c32c5e35b
Avinash <avimalhotra505@gmail.com>
2024-06-29 15:00:06 +0530

    change detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,51 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { ServerComponent } from './server/server.component';
-import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { SearchComponent } from './search/search.component';
-import { ErrorComponent } from './error/error.component';
-import { Routes, RouterModule } from '@angular/router';
-import { RouterOutlet } from '@angular/router';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+//import { ServerComponent } from './server/server.component';
+//import { LoginComponent } from './login/login.component';
+//import { SearchComponent } from './search/search.component';
+//import { ErrorComponent } from './error/error.component';
+//import { Routes, RouterModule } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
+//import { RouterLink, RouterLinkActive } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ FormsModule, CommonModule, ServerComponent, LoginComponent, ErrorComponent, HomeComponent, SearchComponent,RouterModule, RouterOutlet ],
+  //imports: [ FormsModule, CommonModule, ServerComponent, LoginComponent, ErrorComponent, HomeComponent, SearchComponent,RouterModule, RouterOutlet ],
+  imports:[ FormsModule, CommonModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'ngApp';
-  
+  counter="hi";
+  changeCounter(){ this.counter="hello" }
+
+  y=10;
+  changeY(){ this.y = this.y + 10 }
+
+  i=0;
+  data:any;
+
+  constructor(  ){ }
+
+  ngOnInit(){
+     // setTimeout( ()=>{ this.counter = this.counter + 1 }, 2000 );
+      //setInterval( ()=>{ this.counter = this.counter + 1}, 2000 );
+
+      this.i=2;
+      this.data=new BehaviorSubject(this.i);
+  }
+
+  double(){
+    this.i=this.i + this.i;
+    this.data.next(this.i );
+  }
+ 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  @Input() x:number=0;
 
 }
